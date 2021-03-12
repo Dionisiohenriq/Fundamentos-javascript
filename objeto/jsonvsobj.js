4eedeb351f68d6b12520a29724d6422530f43fa7
Henrique Dionisio Ferreira <henriquedionisio.dionisioferre@gmail.com>
2021-03-12 02:40:17 -0300

    Novos exercícios

---
+++ objeto/jsonvsobj.js
@@ -0,0 +1,6 @@
+const obj ={ a: 1, b: 2, c: 3, d: 4, soma(){ return a + b + c + d } }
+console.log(JSON.stringify(obj))
+//console.log(JSON.parse("{ a: 1, b: 2, c: 3, d: 4}"))
+
+//console.log(JSON.parse("{ 'a': 1, 'b': 2, 'c': 3}"))
+console.log(JSON.parse('{ "a": 1, "b": "string", "c": true, "d": {}, "e": [] }'))
