8f4549eb375c9e0eb81ad3c018ef534de0841af0
Henrique Dionisio Ferreira <henriquedionisio.dionisioferre@gmail.com>
2021-03-19 03:28:30 -0300

    Arrays -> concat e flatMap

---
+++ Array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Washington', 'Uesclei']
+
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
